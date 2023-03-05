f29444c78fde8dc3585d98c29a3b93cea1c23bef
TreezCode <TreezCode@gmail.com>
2023-03-05 00:15:17 -0500

    fixed next/image domain issue

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    loader: 'default',
     domains: [
       'cdn.sanity.io',
+      'vercel',
+      'vercel.app',
+      'treecommerce',
+      'treecommerce.vercel',
       'treecommerce.vercel.app',
       'treecommerce-treezcode.vercel.app',
       'treecommerce-git-main-treezcode.vercel.app',
     ],
+    reactStrictMode: true,
     formats: ['image/webp'],
     unoptimized: true,
   },
